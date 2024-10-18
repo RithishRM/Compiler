d87313c0dda83e5f5f9f938299ee8fbf7663c1df
RithishRM <pes2ug23cs458@pesu.pes.edu>
2024-10-18 22:34:51 +0530

    Updated issue #2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,62 @@
 import axios from 'axios';
 import './App.css';
-import React, {useState} from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
-
   const [code, setCode] = useState('');
   const [language, setLanguage] = useState('cpp');
   const [output, setOutput] = useState('');
-  const handleSubmit = async ()=>{
-    const payload = {
-      language,
-      code
-    };
-    try{
-    const { data }= await axios.post("http://localhost:8081/run",payload)
-    setOutput(data.output);}catch(err){
+  const [theme, setTheme] = useState('light');
+
+  const handleSubmit = async () => {
+    const payload = { language, code };
+    try {
+      const { data } = await axios.post("http://localhost:8081/run", payload);
+      setOutput(data.output);
+    } catch (err) {
       console.log(err.response);
     }
-  }
+  };
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.body.className = theme; 
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
-    <div className="App">
-      <h1>Online code compiler</h1>
+    <div className={`App ${theme}`}>
+      <h1>Online Code Compiler</h1>
+
+      <div className="toggle-switch">
+        <input
+          type="checkbox"
+          id="theme-toggle"
+          onChange={toggleTheme}
+          checked={theme === 'dark'} 
+        />
+        <label htmlFor="theme-toggle"></label>
+      </div>
+
       <div>
-        <label>Language:  </label>
-        <select value = {language} onChange={(e)=>{
-        setLanguage(e.target.value);
-        console.log(e.target.value);
-        }}>
+        <label>Language: </label>
+        <select value={language} onChange={(e) => setLanguage(e.target.value)}>
           <option value="cpp">C++</option>
           <option value="py">Python</option>
         </select>
       </div>
-      <br/>
-      <textarea rows="20" cols = "75" value = {code} onChange={(e)=>{setCode(e.target.value);}}></textarea>
-      <br/>
+      <br />
+      <textarea
+        className={`code-editor ${theme}`} 
+        rows="20"
+        cols="75"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      ></textarea>
+      <br />
       <button onClick={handleSubmit}>Submit</button>
       <p>{output}</p>
     </div>
